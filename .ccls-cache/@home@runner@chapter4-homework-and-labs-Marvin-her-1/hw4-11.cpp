bbcbff44f553995213197fb2e85f5b109d5012d5
Marvin-her <marvin321her@icloud.com>
2024-10-03 19:51:09 +0000

    Homework 4-11

--- .ccls-cache/@home@runner@chapter4-homework-and-labs-Marvin-her-1/hw4-11.cpp
+++ .ccls-cache/@home@runner@chapter4-homework-and-labs-Marvin-her-1/hw4-11.cpp
@@ -5,7 +5,18 @@
 using namespace std;
 
 int main() {
-  
-
+  double beginingBalance = 0.0;
+  double totalDeposits = 0.0;
+  double totalWithdrawls = 0.0;
+  double finalBalance = 0.0;
+  cout << "Begining balance: ";
+  cin >> beginingBalance;
+  cout << "Total deposits: ";
+  cin >> totalDeposits;                    
+  cout << "Total withdrawls: ";
+  cin >> totalWithdrawls;
+  finalBalance = beginingBalance + totalDeposits - totalWithdrawls;
+  cout << "Final balance: " << finalBalance << endl;
   return 0;
+
 } // end of main function
